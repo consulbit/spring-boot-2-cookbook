36e4f3fd715e790ce9822f7a27a036f43869a629
Pietro Cascio <piero.cascio@gmail.com>
2019-01-30 16:32:49 +0100

    Custom Servlet Filters

---
+++ src/main/java/com/example/bookpub/config/WebConfiguration.java
@@ -0,0 +1,15 @@
+package com.example.bookpub.config;
+
+import org.apache.catalina.filters.RemoteIpFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfiguration {
+
+  @Bean
+  public RemoteIpFilter remoteIpFilter() {
+    return new RemoteIpFilter();
+  }
+
+}
