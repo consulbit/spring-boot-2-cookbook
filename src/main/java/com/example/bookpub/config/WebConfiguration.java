d57ceeec599af5b59cc7e45d185e3bb5520d39bc
Pietro Cascio <piero.cascio@gmail.com>
2019-01-30 20:29:01 +0100

    Custom Http Message Converters.

--- src/main/java/com/example/bookpub/config/WebConfiguration.java
+++ src/main/java/com/example/bookpub/config/WebConfiguration.java
@@ -1,8 +1,11 @@
 package com.example.bookpub.config;
 
+import java.util.List;
 import org.apache.catalina.filters.RemoteIpFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.ByteArrayHttpMessageConverter;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
@@ -20,11 +23,24 @@ public class WebConfiguration implements WebMvcConfigurer {
     return new LocaleChangeInterceptor();
   }
 
+  @Bean
+  public ByteArrayHttpMessageConverter byteArrayHttpMessageConverter() {
+    return new ByteArrayHttpMessageConverter();
+  }
+
   @Override
   public void addInterceptors(InterceptorRegistry registry) {
     registry.addInterceptor(localeChangeInterceptor());
   }
 
+  @Override
+  public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+    converters.add(byteArrayHttpMessageConverter());
+  }
 
-
+//  @Override
+//  public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+//    converters.clear();
+//    converters.add(byteArrayHttpMessageConverter());
+//  }
 }
